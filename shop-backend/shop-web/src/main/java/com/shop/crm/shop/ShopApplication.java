c6cd47d394fdc021abbab99ffdfc9d66553f16b3
Tim <s1032030@g.ncyu.edu.tw>
2022-02-20 09:29:10 +0800

    [dev][模組: 無][說明: 建立專案 ]
    1. 建立專案

---
+++ shop-backend/shop-web/src/main/java/com/shop/crm/shop/ShopApplication.java
@@ -0,0 +1,12 @@
+package com.shop.crm.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShopApplication.class, args);
+    }
+}
